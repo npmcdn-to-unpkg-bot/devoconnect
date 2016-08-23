c103a47a981482aa0897b5d7efce0a44328761f7
Ben Read <endymion1818@gmail.com>
2016-08-23 14:37:34 +0100

    replaced events cpt

--- functions.php
+++ functions.php
@@ -311,6 +311,7 @@ remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 // Custom Post Types
 function create_posttype() {
+
   register_post_type( 'network',
     array(
       'labels' => array(
@@ -321,6 +322,18 @@ function create_posttype() {
       'has_archive' => false,
       'supports' => array('title', 'editor', 'thumbnail'),
       'rewrite' => array('slug' => 'network')
+    )
+  );
+  register_post_type( 'events',
+    array(
+      'labels' => array(
+        'name' => __( 'Events' ),
+        'singular_name' => __( 'Event' )
+      ),
+      'public' => true,
+      'has_archive' => false,
+      'supports' => array('title', 'editor', 'thumbnail'),
+      'rewrite' => array('slug' => 'events')
     )
   );
 }
